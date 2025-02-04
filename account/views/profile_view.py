26b8e196bb195e8c532693e4a8f993385ef26307
pochimoritaro <pochimoritaro@gmail.com>
2025-02-04 22:25:08 +0900

    プロフィール用のビューにロギングを追加

--- account/views/profile_view.py
+++ account/views/profile_view.py
@@ -1,10 +1,16 @@
+import logging
 from django.views.generic import TemplateView
+
+logger = logging.getLogger("account.profile")
 
 class ProfileView(TemplateView):
     """ ユーザー登録用ビュー """
     template_name = "account/profile.html"
 
     def get_context_data(self, **kwargs):
+        logger.debug(f"{self.__class__.__name__}の処理を開始します")
         context = super().get_context_data(**kwargs)
+        logger.debug(f"コンテキストデータ\n\t{context}")
         context["profile"] = self.request.user
+        logger.debug(f"プロフィールデータ挿入後のコンテキストデータ\n\t{context}")
         return context
