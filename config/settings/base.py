68031e73df4038c9e7eaec87b7a248ab75d0a491
pochimoritaro <pochimoritaro@gmail.com>
2025-02-04 22:23:59 +0900

    ロギングフォーマットの基本設定を修正

--- config/settings/base.py
+++ config/settings/base.py
@@ -54,7 +54,7 @@ LOGGING = {
     "disable_existing_loggers": False,
     "formatters": {
         "verbose": {
-            "format": "[{levelname}] {asctime} {name}: {message}",
+            "format": "[{levelname}] {asctime} [{name}] {message}",
             "style": "{",
         },
     },
